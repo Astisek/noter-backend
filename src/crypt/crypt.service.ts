0a757009301960a1949b565ba33fe688ed294964
Astisek <Astisek@hotmail.com>
2025-02-10 21:31:19 +0500

    changed bcrypt to bcriptjs

--- src/crypt/crypt.service.ts
+++ src/crypt/crypt.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IConfig } from 'src/interfaces/config';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class CryptService {
